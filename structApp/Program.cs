fd40dae650c53be1c61175c31077728bd313ecbf
jesse collier <jesse.collier97@gmail.com>
2019-06-20 14:35:05 +1200

    comparing struct to clases

--- structApp/Program.cs
+++ structApp/Program.cs
@@ -4,9 +4,17 @@ namespace structApp
 {
     class Program
     {
+        static void MultipleLegs(PetStruct petStruct, PetClass petClass)
+        {
+            petStruct.Legs = petStruct.Legs * 2;
+            petClass.Legs = petClass.Legs * 2;
+
+            Console.WriteLine("Internal Method - A " + petStruct.Type + " Has " + petStruct.Legs + " Legs");
+            Console.WriteLine("Internal Method - A " + petClass.Type + " Has " + petClass.Legs + " Legs");
+        }
         static void Main(string[] args)
         {
-            PetStruct dog;
+            PetStruct dog = new PetStruct();
             dog.Type = PetType.Dog;
             dog.HasFur = true;
             dog.Legs = 4;
@@ -18,6 +26,13 @@ namespace structApp
 
             Console.WriteLine("a " + dog.Type + " has " + dog.Legs + " Legs");
             Console.WriteLine("a " + duck.Type + " has " + duck.Legs + " Legs");
+
+
+            MultipleLegs(dog, duck);
+
+            Console.WriteLine("a " + dog.Type + " has " + dog.Legs + " Legs");
+            Console.WriteLine("a " + duck.Type + " has " + duck.Legs + " Legs");
+
             Console.ReadLine();
         }
     }
