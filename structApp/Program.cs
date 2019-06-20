feea0592ef2bcd53f0e8ccfdde28165ba616214b
jesse collier <jesse.collier97@gmail.com>
2019-06-20 14:19:36 +1200

    Add project files.

---
+++ structApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace structApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PetStruct dog;
+            dog.Type = PetType.Dog;
+            dog.HasFur = true;
+            dog.Legs = 4;
+
+            PetClass duck = new PetClass();
+            duck.Type = PetType.Duck;
+            duck.HasFur = false;
+            duck.Legs = 2;
+
+            Console.WriteLine("a " + dog.Type + " has " + dog.Legs + " Legs");
+            Console.WriteLine("a " + duck.Type + " has " + duck.Legs + " Legs");
+            Console.ReadLine();
+        }
+    }
+
+    class PetClass
+    {
+        public int Legs;
+        public PetType Type;
+        public string Name;
+        public bool HasFur;
+    }
+
+    struct PetStruct
+    {
+        public int Legs;
+        public PetType Type;
+        public string Name;
+        public bool HasFur;
+    }
+
+    enum PetType
+    {
+        Dog,
+        Duck
+    }
+}
